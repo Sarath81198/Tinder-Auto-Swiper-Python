cbf4c0ac0f50f941d5076d53dc6dd01ec4d4b6de
Sarath Dev <sarathsekaran2007@gmail.com>
2020-02-18 22:52:13 +0530

    Auto Swiper added

---
+++ bot.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from time import sleep
+
+class TinderBot():
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+
+    def start_tinder(self):
+        self.driver.get('https://tinder.com')
+
+
+    def like(self):
+        like_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[3]')
+        like_btn.click()
+
+    def dislike(self):
+        dislike_btn = self.driver.find_element_by_xpath('//*[@id="content"]/div/div[1]/div/main/div[1]/div/div/div[1]/div/div[2]/button[1]')
+        dislike_btn.click()
+
+    def auto_swipe(self):
+        self.start_tinder()
+        while True:
+            sleep(1)
+            try:
+                self.like()
+            except Exception:
+                try:
+                    self.close_popup()
+                except Exception:
+                    self.close_match()
+
+    def close_popup(self):
+        popup_3 = self.driver.find_element_by_xpath('//*[@id="modal-manager"]/div/div/div[2]/button[2]')
+        popup_3.click()
+
+    def close_match(self):
+        match_popup = self.driver.find_element_by_xpath('//*[@id="modal-manager-canvas"]/div/div/div[1]/div/div[3]/a')
+        match_popup.click()
+
+bot = TinderBot()
+bot.auto_swipe()
